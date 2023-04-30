5fce6dda0071725c7cecd9f3e2b33110bc5810da
DeepanshuRohilla15 <20001001027deepanshu@dcrustm.org>
2023-04-30 22:57:22 +0530

    Initialized

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyCSAd5VUZqK_uq_5KdP8HXLmCM03aBAmeU",
+      authDomain: "clone-c41fe.firebaseapp.com",
+      projectId: "clone-c41fe",
+      storageBucket: "clone-c41fe.appspot.com",
+      messagingSenderId: "1094303296426",
+      appId: "1:1094303296426:web:274e53cfcc35cde5d0d9d3"
+    };
