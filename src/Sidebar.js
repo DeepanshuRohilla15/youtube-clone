112b0b608ad18c98365e26c95a66f0c278f6eb9d
DeepanshuRohilla15 <20001001027deepanshu@dcrustm.org>
2023-05-06 13:13:30 +0530

    Build sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,12 @@ import SidebarRow from "./SidebarRow";
 import HomeIcon from '@mui/icons-material/Home';
 import WhatshotIcon from '@mui/icons-material/Whatshot';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import HistoryIcon from '@mui/icons-material/History';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 function Sidebar() {
   return (
@@ -12,6 +18,17 @@ function Sidebar() {
       <SidebarRow Icon={WhatshotIcon} title="Trending"/>
       <SidebarRow Icon={SubscriptionsIcon} title="Subscription"/>
       <hr />
+      <SidebarRow Icon={VideoLibraryIcon} title="Library" />
+      <SidebarRow Icon={HistoryIcon} title="History" />
+      <SidebarRow Icon={OndemandVideoIcon} title="Your videos" />
+      <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+      <SidebarRow Icon={ThumbUpOffAltIcon} title="Liked videos" />
+      <SidebarRow Icon={ExpandMoreIcon} title="Show more" />
+
+
+
+
+
     </div>
   )
 }
